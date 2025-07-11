735b49d05b07f63fd9b456c768fefb6edd7a75ae
unknown <d3rgluck@yandex.ru>
2025-07-11 21:13:04 +0300

    add input attack, climb, jump initial handling

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,24 +3,51 @@ extends CharacterBody2D
 #player movement variables
 @export var speed = 100
 @export var gravity = 200
+@export var jump_height = -100
+var is_attacking = false
+var is_climbing = false
 
 #movement and physics
 func _physics_process(delta):
 	# vertical movement velocity (down)
-	velocity.y += gravity * delta
+	#velocity.y += gravity * delta
 	# horizontal movement processing (left, right)
 	horizontal_movement()
+	
 	#applies movement
-	#move_and_slide() 
+	move_and_slide() 
+	
 	#applies animations
-	player_animations()
+	if !is_attacking:
+		player_animations()
 	
 func horizontal_movement():
 	# if keys are pressed it will return 1 for ui_right, -1 for ui_left, and 0 for neither
 	var horizontal_input = Input.get_action_strength("ui_right") -  Input.get_action_strength("ui_left")
 	# horizontal velocity which moves player left or right based on input
 	velocity.x = horizontal_input * speed
- 
+	
+#singular input captures
+func _input(event):
+	#on attack
+	if event.is_action_pressed("ui_attack"):
+		is_attacking = true
+		$AnimatedSprite2D.play("attack1")
+	#on jump
+	if event.is_action_pressed("ui_jump") and is_on_floor():
+		velocity.y = jump_height
+		$AnimatedSprite2D.play("jump")
+	#on climbing ladders
+	if is_climbing == true:
+		if Input.is_action_pressed("ui_up"):
+			$AnimatedSprite2D.play("climb") 
+			gravity = 100
+			velocity.y = -200
+	#reset gravity
+	else:
+		gravity = 200
+		is_climbing = false
+
 #animations
 func player_animations():
 	#on left (add is_action_just_released so you continue running after jumping)
@@ -32,3 +59,11 @@ func player_animations():
 	if Input.is_action_pressed("ui_right") || Input.is_action_just_released("ui_jump"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("run")
+		
+	#on idle if nothing is being pressed
+	if !Input.is_anything_pressed():
+		$AnimatedSprite2D.play("idle")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	is_attacking = false
